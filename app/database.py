cf666bfa9e78217d1234d94b675383fc65e8d600
Mahmood Heidari <mahmoodh1378@gmail.com>
2021-06-07 10:56:00 +0430

    ✅ added get_db function in database

--- app/database.py
+++ app/database.py
@@ -2,13 +2,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+SQLALCHAMY_DATABASE_URL = 'sqlite:///./blog.db'
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+engine = create_engine(SQLALCHAMY_DATABASE_URL, connect_args={
+                       "check_same_thread": False})
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False,)
 
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
