530664d183a22858c2d7ce0124e81add08deebe3
Mahmood Heidari <mahmoodh1378@gmail.com>
2021-06-09 23:19:11 +0430

    ✅ models added

--- app/models.py
+++ app/models.py
@@ -1,23 +1,25 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, ForeignKey
 from .database import Base
-
-
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
-    password = Column(String)
-
-    posts = relationship("Post", back_populates="user")
+from sqlalchemy.orm import relationship
 
 
 class Post(Base):
-    __tablename__ = "posts"
+    __tablename__ = 'posts'
+
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    context = Column(String, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"))
+    title = Column(String)
+    context = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
 
     user = relationship("User", back_populates="posts")
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True)
+    email = Column(String, unique=True)
+    password = Column(String)
+
+    posts = relationship('Post', back_populates="user")
